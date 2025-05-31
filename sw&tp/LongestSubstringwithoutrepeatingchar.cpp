07b9bbb56f09295ef837dc231940ffa98e3f1656
Chirag vijayvergiya <111374446+chirag412vijayvergiya@users.noreply.github.com>
2025-05-31 11:16:05 +0530

    tc:- O(2*N) and sc:- O(N)

--- sw&tp/LongestSubstringwithoutrepeatingchar.cpp
+++ sw&tp/LongestSubstringwithoutrepeatingchar.cpp
@@ -2,20 +2,41 @@
 #include <unordered_set>
 using namespace std;
 
+// int solution(string s){
+//     if(s.size() == 0) return 0;
+//     int maxLen = 0;
+// for(int i = 0; i < s.length(); i++){
+//         unordered_set<int>set;
+
+//         for(int j = i; j < s.length(); j++){
+//             if(set.find(s[j]) != set.end()){
+//                 maxLen = max(maxLen, j - i);
+//                 break;
+//             }
+
+//             set.insert(s[j]);
+//         }       
+//     }
+
+//     return maxLen;
+// }
+
 int solution(string s){
     if(s.size() == 0) return 0;
     int maxLen = 0;
-for(int i = 0; i < s.length(); i++){
-        unordered_set<int>set;
+    int l = 0;
+    unordered_set<int>set;
+    for(int r = 0; r < s.length(); r++){
 
-        for(int j = i; j < s.length(); j++){
-            if(set.find(s[j]) != set.end()){
-                maxLen = max(maxLen, j - i);
-                break;
+        if(set.find(s[r]) != set.end()){
+            while(l < r && set.find(s[r]) != set.end()){
+                set.erase(s[l]);
+                l++;
             }
-
-            set.insert(s[j]);
-        }       
+        }
+        
+        maxLen = max(maxLen, r - l + 1);
+        set.insert(s[r]);
     }
 
     return maxLen;
