7ebd6fc48169e43ad65b63d5ac278077e4b3af8c
Chirag vijayvergiya <111374446+chirag412vijayvergiya@users.noreply.github.com>
2025-06-30 01:42:54 +0530

    Half Diamond Pattern

---
+++ Patterns/Half_Diamond_Pattern.cpp
@@ -0,0 +1,29 @@
+// * 
+// * * 
+// * * * 
+// * * * * 
+// * * * * * 
+// * * * * 
+// * * * 
+// * * 
+// * 
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n = 5;
+    for(int i = 1; i <= n * 2 - 1; i++){
+        if(i <= n){
+            for(int j = 1; j <= i; j++){
+                cout<<"*"<<" ";
+            }
+        }else{
+            for(int j = 1; j <= n*2 - i; j++){
+                cout<<"*"<<" ";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
